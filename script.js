ee988b9900cf9e3f046093db38452a2f7eaf33a8
velas17 <72086575+velas17@users.noreply.github.com>
2020-11-02 23:20:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// VERY EASY CHALLENGE
+let edar = 4;
+let velasquez = 17;
+console.log('The difference between ' + velasquez + ' and ' + edar + ' is 13');
+
+//EASY
+let lalo = "Eduardo"
+let kique = "Enrique"
+console.log("The name's " + lalo + ' and ' + kique + ' are equal and have the same number of characters.')
+
+//MEDIUM
+let text = prompt('Type a message')
+
+if (text = text.toUpperCase()) {
+    console.log(`Stop Yelling`)
+} else if (text = text.toLowerCase()) {
+    console.log(`Speak up I can't hear you`)
+} else {
+    console.log(`Can't tell if your yellin or whispering`)
+}
+// I am confused idk if it's the browser but two prompt windows pop up back to back
+
+//HARD
+//Addition
+function add(number1, number2) {
+    let additionTotal = number1 + number2;
+    return additionTotal;
+}
+//Subtraction
+function subtract(number1, number2) {
+    let subtractionTotal = number1 - number2;
+    return subtractionTotal;
+};
+//Multiplication
+function multiply(number1, number2) {
+    let multiplicationTotal = number1 * number2;
+    return multiplicationTotal;
+};
+//Division
+function division(number1, number2) {
+    let divisionTotal = number1 / number2;
+    return divisionTotal;
+}
+
+//VERY HARD
+// I got stumped
